74ff0ac2d35bc56dbc99397890d4bc2a5fee0416
MichelFloyd <michel.floyd@cloaklabs.com>
2017-09-23 14:02:17 -0700

    Dealing with the user state for demo

--- client/referral.js
+++ client/referral.js
@@ -6,6 +6,7 @@ import { Invitations } from '../imports/api/invitations/schema.js';
 
 Template.referral.onCreated(function() {
   Tracker.autorun(() => {
+    Meteor.logout(); // for demo purposes - switch context to be the invitee
     Meteor.subscribe('oneInvitation',FlowRouter.getParam('invitationId'));
   });
 });
@@ -14,4 +15,10 @@ Template.referral.helpers({
   invitation() {
     return Invitations.findOne(FlowRouter.getParam('invitationId'));
   },
+});
+
+Template.referral.events({
+  'click #login'(ev) {
+    Meteor.loginWithPassword('foo@bar.com', 'ph00bar');
+  }
 });
